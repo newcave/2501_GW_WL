246b963ab9c22e532f541cc889f59d31f6374325
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2025-01-20 10:55:53 +0900

    Update 2501_GW_WL.py

--- 2501_GW_WL.py
+++ 2501_GW_WL.py
@@ -109,6 +109,8 @@ else:
     st.info("💡 **K-water AI LAB x Groundwater Research Team Collaboration.**")
     col1, col2 = st.columns(2)
     with col1:
-        st.image("FIG2.png", caption="📍 위치도 및 염분 분포도")
+        st.image("FIG2.png", caption="📍 분석 개념도")
+
     with col2:
-        st.video("media.mp4", caption="📍 위치도 및 염분 분포도")
+        st.video("media.mp4")
+        st.markdown("📍 위치도 및 염분 분포도")
