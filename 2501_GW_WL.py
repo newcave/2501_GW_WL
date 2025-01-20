acd46eef6006d8cba6721ef9d2af14492ff1bb3c
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2025-01-20 10:57:28 +0900

    Update 2501_GW_WL.py

--- 2501_GW_WL.py
+++ 2501_GW_WL.py
@@ -110,7 +110,7 @@ else:
     col1, col2 = st.columns(2)
     with col1:
         st.image("FIG2.png", caption="📍 분석 개념도")
-        st.markdown("** 주요관측정 자료 기반 예측 수행**"")
+        st.markdown("** 주요관측정 자료 기반 예측 수행**")
     with col2:
         st.video("media.mp4")
         st.markdown("📍 위치도 및 염분 분포도")
